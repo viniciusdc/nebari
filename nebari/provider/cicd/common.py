dd72af5cdca0fe2c605ffd38a7470fa0986fa2e7
vinicius douglas cerutti <vinivdc2009@hotmail.com>
2023-08-17 23:33:41 -0300

    include option to pip install from forked repos

--- nebari/provider/cicd/common.py
+++ nebari/provider/cicd/common.py
@@ -3,9 +3,12 @@ import os
 
 def pip_install_nebari(nebari_version: str) -> str:
     nebari_gh_branch = os.environ.get("NEBARI_GH_BRANCH", None)
+    nebari_source_repo = os.environ.get(
+        "NEBARI_SOURCE_REPO", "https://github.com/nebari-dev/nebari.git"
+    )
     pip_install = f"pip install nebari=={nebari_version}"
     # dev branches
     if nebari_gh_branch:
-        pip_install = f"pip install git+https://github.com/nebari-dev/nebari.git@{nebari_gh_branch}"
+        pip_install = f"pip install git+{nebari_source_repo}@{nebari_gh_branch}"
 
     return pip_install
